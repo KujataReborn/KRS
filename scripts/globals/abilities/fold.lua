b5fa7e2037656e3775d341a92be620f44ba2960f
BillBows <william.henrique@outlook.com>
2020-04-18 18:26:23 -0300

    Fold
    - Removing out of era functionality

--- scripts/globals/abilities/fold.lua
+++ scripts/globals/abilities/fold.lua
@@ -2,7 +2,7 @@
 -- Ability: Fold
 -- Erases one roll or bust tpz.effect. Targets self-cast effect with the longest remaining duration.
 -- Obtained: Corsair Level 75
--- Recast Time: 00:05:00
+-- Recast Time: 00:15:00
 -- Duration: Instant
 -----------------------------------
 require("scripts/globals/settings")
@@ -11,7 +11,7 @@ require("scripts/globals/msg")
 -----------------------------------
 
 function onAbilityCheck(player,target,ability)
-    if (target:hasCorsairEffect()) then
+    if target:hasCorsairEffect() then
         return 0,0
     else
         return tpz.msg.basic.CANNOT_PERFORM,0
@@ -20,13 +20,6 @@ end
 
 function onUseAbility(player,target,ability)
     target:fold()
-
-    local merit = target:getMerit(tpz.merit.FOLD)
-    merit = merit - 10
-
-    if (math.random(0, 99) < merit) then
-        target:resetRecast(tpz.recast.ABILITY,193)
-    end
 
     return tpz.effect.FOLD
 end
