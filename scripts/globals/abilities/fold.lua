d5077c02876702e8f4753035f5bd57162270515d
BillBows <william.henrique@outlook.com>
2020-04-24 14:52:48 -0300

    Addressing merit upgrades to affect merited JA recast timers (COR)

--- scripts/globals/abilities/fold.lua
+++ scripts/globals/abilities/fold.lua
@@ -5,12 +5,18 @@
 -- Recast Time: 00:15:00
 -- Duration: Instant
 -----------------------------------
-require("scripts/globals/settings")
 require("scripts/globals/status")
 require("scripts/globals/msg")
 -----------------------------------
 
 function onAbilityCheck(player,target,ability)
+    local merit = player:getMerit(tpz.merit.FOLD)
+    if merit >= 2 then
+        local reduction = (merit - 1) * 150 -- 30 seconds for each merit upgrade after unlocking
+
+        ability:setRecast(ability:getRecast() - reduction)
+    end
+
     if target:hasCorsairEffect() then
         return 0,0
     else
--- scripts/globals/abilities/snake_eye.lua
+++ scripts/globals/abilities/snake_eye.lua
@@ -5,16 +5,22 @@
 -- Recast Time: 0:15:00
 -- Duration: 0:01:00 or the next usage of Phantom Roll or Double-Up
 -----------------------------------
-require("scripts/globals/settings")
 require("scripts/globals/status")
 -----------------------------------
 
 function onAbilityCheck(player,target,ability)
+    local merit = player:getMerit(tpz.merit.SNAKE_EYE)
+    if merit >= 2 then
+        local reduction = (merit - 1) * 150 -- 30 seconds for each merit upgrade after unlocking
+
+        ability:setRecast(ability:getRecast() - reduction)
+    end
+
     return 0,0
 end
 
 function onUseAbility(player,target,ability)
-    player:addStatusEffect(tpz.effect.SNAKE_EYE,(player:getMerit(tpz.merit.SNAKE_EYE) - 5),0,60)
+    player:addStatusEffect(tpz.effect.SNAKE_EYE, 1, 0, 60)
 
     return tpz.effect.SNAKE_EYE
 end
