b3cf0108794402b2feea1df292c2d5e0fe2c482b
BillBows <william.henrique@outlook.com>
2020-04-25 18:32:28 -0300

    Hasso
    - Removing retail functionality
    - Removing unnecessary requires
    - Formatting

--- scripts/globals/effects/hasso.lua
+++ scripts/globals/effects/hasso.lua
@@ -1,10 +1,12 @@
 -----------------------------------
--- Hasso
--- Straight +10% haste +10 Acc and scaling (lv) STR
--- also -50% FC
+--
+-- tpz.effect.HASSO
+--
 -----------------------------------
 require("scripts/globals/status")
-function onEffectGain(target, effect)
+-----------------------------------
+
+function onEffectGain(target,effect)
     target:addMod(tpz.mod.STR, effect:getPower())
     target:addMod(tpz.mod.HASTE_ABILITY, 1000)
     target:addMod(tpz.mod.ACC, 10)
@@ -13,7 +15,7 @@ end
 function onEffectTick(target, effect)
 end
 
-function onEffectLose(target, effect)
+function onEffectLose(target,effect)
     target:delMod(tpz.mod.STR, effect:getPower())
     target:delMod(tpz.mod.HASTE_ABILITY, 1000)
     target:delMod(tpz.mod.ACC, 10)
