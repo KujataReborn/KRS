c9703c3b2a69f5e285c2fd8b715df235e6b8a24b
Ranzera <35186045+Ranzera@users.noreply.github.com>
2020-04-25 19:59:09 -0800

    Modernizing - Missed a spot

--- scripts/zones/Aht_Urhgan_Whitegate/npcs/Gathweeda.lua
+++ scripts/zones/Aht_Urhgan_Whitegate/npcs/Gathweeda.lua
@@ -4,15 +4,15 @@
 --  Guild Merchant: Alchemist Guild
 -- !pos -81.322 -6.000 140.273 50
 -----------------------------------
-local ID = require("scripts/zones/Aht_Urhgan_Whitegate/IDs")
 require("scripts/globals/settings")
 require("scripts/globals/shop")
+local ID = require("scripts/zones/Aht_Urhgan_Whitegate/IDs")
 -----------------------------------
 
 function onTrade(player, npc, trade)
 end
 
-function onTrigger(player,npc)
+function onTrigger(player, npc)
     if player:sendGuild(60431, 8, 23, 6) then
         player:showText(npc, ID.text.GATHWEEDA_SHOP_DIALOG)
     end
--- scripts/zones/Aht_Urhgan_Whitegate/npcs/Wahraga.lua
+++ scripts/zones/Aht_Urhgan_Whitegate/npcs/Wahraga.lua
@@ -4,15 +4,15 @@
 --  Guild Merchant: Alchemist Guild
 -- !pos -76.836 -6.000 140.331 50
 -----------------------------------
-local ID = require("scripts/zones/Aht_Urhgan_Whitegate/IDs")
 require("scripts/globals/settings")
 require("scripts/globals/shop")
+local ID = require("scripts/zones/Aht_Urhgan_Whitegate/IDs")
 -----------------------------------
 
 function onTrade(player, npc, trade)
 end
 
-function onTrigger(player,npc)
+function onTrigger(player, npc)
     if player:sendGuild(60432, 8, 23, 6) then
         player:showText(npc, ID.text.WAHRAGA_SHOP_DIALOG)
     end
