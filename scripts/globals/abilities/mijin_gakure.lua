9471e18b24843d8816fff317f8e9981ed452e66d
BillBows <william.henrique@outlook.com>
2020-05-26 18:47:18 -0300

    Mijin Gakure
    - Formatting
    - Removing unnecessary requires

--- scripts/globals/abilities/mijin_gakure.lua
+++ scripts/globals/abilities/mijin_gakure.lua
@@ -2,11 +2,10 @@
 -- Ability: Mijin Gakure
 -- Sacrifices user's life to deal damage to an enemy.
 -- Obtained: Ninja Level 1
--- Recast Time: 1:00:00
+-- Recast Time: 02:00:00
 -- Duration: Instant
 -----------------------------------
 require("scripts/globals/monstertpmoves")
-require("scripts/globals/settings")
 require("scripts/globals/status")
 -----------------------------------
 
@@ -15,9 +14,8 @@ function onAbilityCheck(player,target,ability)
 end
 
 function onUseAbility(player,target,ability)
-
     local dmg = (player:getHP() * 0.8) + (player:getMainLvl() / 0.5)
-    local resist = applyPlayerResistance(player, nil, target, player:getStat(tpz.mod.INT)-target:getStat(tpz.mod.INT), 0, tpz.magic.ele.NONE)
+    local resist = applyPlayerResistance(player, nil, target, player:getStat(tpz.mod.INT) - target:getStat(tpz.mod.INT), 0, tpz.magic.ele.NONE)
 
     dmg = dmg * resist
 
@@ -26,5 +24,6 @@ function onUseAbility(player,target,ability)
 
     player:setLocalVar("MijinGakure", 1)
     player:setHP(0)
+
     return dmg
 end
