abb25598ff44a7f8de3c063848398f97d8031673
BillBows <william.henrique@outlook.com>
2020-06-02 18:23:29 -0300

    Monomi
    - Formatting

--- scripts/globals/spells/monomi_ichi.lua
+++ scripts/globals/spells/monomi_ichi.lua
@@ -6,17 +6,18 @@
 require("scripts/globals/settings")
 require("scripts/globals/status")
 require("scripts/globals/msg")
+-----------------------------------------
 
 function onMagicCastingCheck(caster,target,spell)
     return 0
 end
 
 function onSpellCast(caster,target,spell)
-    if (not target:hasStatusEffect(tpz.effect.SNEAK)) then
+    if not target:hasStatusEffect(tpz.effect.SNEAK) then
         spell:setMsg(tpz.msg.basic.MAGIC_GAIN_EFFECT)
         target:addStatusEffect(tpz.effect.SNEAK, 0, 10, math.floor(420 * SNEAK_INVIS_DURATION_MULTIPLIER))
     else
-        spell:setMsg(tpz.msg.basic.MAGIC_NO_EFFECT) -- no tpz.effect.
+        spell:setMsg(tpz.msg.basic.MAGIC_NO_EFFECT)
     end
 
     return tpz.effect.SNEAK
