41cd63354cb8b56bfe9e155a66b2ad07fad17248
BillBows <william.henrique@outlook.com>
2020-06-11 18:38:33 -0300

    Fixing Fraelissa's typo

--- scripts/zones/Jugner_Forest/mobs/Fraelissa.lua
+++ scripts/zones/Jugner_Forest/mobs/Fraelissa.lua
@@ -12,5 +12,5 @@ end
 function onMobDespawn(mob)
     UpdateNMSpawnPoint(mob:getID())
     mob:setRespawnTime(math.random(3600, 4500)) -- 60 to 75 minutes
-    tpz.mob.phOnDespawn(mob, ID.mob.FRADUBIO_PH, 10, math.random(75600, 86400)) then -- 21-24 hours
+    tpz.mob.phOnDespawn(mob, ID.mob.FRADUBIO_PH, 10, math.random(75600, 86400)) -- 21-24 hours
 end
